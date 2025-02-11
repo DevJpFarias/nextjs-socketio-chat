27d2a315f24509d8208b4bbb97a84466c456e3d7
DevJpFarias <joaopfc2764@gmail.com>
2025-02-11 15:19:46 -0300

    feat: redirecionando ao entrar no site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+import { useEffect } from "react";
+
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/chat');
+  }, [router]);
+
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className='min-h-dvh flex items-center justify-center bg-slate-900'>
+      <h1 className='text-white text-xl'>Carregando...</h1>
     </div>
   );
 }
