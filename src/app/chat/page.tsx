7b47271c168d132da09b865ca06a2074e843eeb8
DevJpFarias <joaopfc2764@gmail.com>
2025-02-11 14:40:31 -0300

    fix: tornando a página default

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,7 +1,7 @@
 import ChatLayout from "@/layouts/ChatLayout";
 import { Chat } from "./components/Chat";
 
-export function ChatPage() {
+export default function ChatPage() {
   return (
     <ChatLayout>
       <Chat />
